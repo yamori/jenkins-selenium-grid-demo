de78184168a2bd79e6c5f34c2154f6ed7c35acae
Kindzerske <matthew.kindzerske@accenturefederal.com>
2017-03-22 14:14:50 -0400

    Building on example selenium tests.

--- src/test/java/org/kay/jenk_selenium_hello/WikipediaTest.java
+++ src/test/java/org/kay/jenk_selenium_hello/WikipediaTest.java
@@ -1,6 +1,9 @@
 package org.kay.jenk_selenium_hello;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+
 import junit.framework.TestCase;
 
 public class WikipediaTest extends TestCase {
@@ -17,6 +20,9 @@ public class WikipediaTest extends TestCase {
 		String pageTitle = driver.getTitle();
 		System.out.println(pageTitle);
 		assertEquals(pageTitle, "Wikipedia, the free encyclopedia");
+		
+		driver.findElement(By.id("searchInput")).sendKeys("Java");
+		driver.findElement(By.id("searchInput")).sendKeys(Keys.RETURN);
 
 		driver.quit();
 	}
